1a3ac75bb8bc1158095b934826eaa0fd171a9b9b
felister kiragu <kiragufelister18@gmail.com>
2024-10-04 20:27:23 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// Array where the total sum is 31
+const batteryBatches = [5, 6, 4, 8, 3, 5];
+
+// Using reduce to sum the battery counts
+const totalBatteries = batteryBatches.reduce((total, batch) => total + batch, 0);
+
+// Testing framework (using Mocha and Chai)
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+describe('reducer', function() {
+  describe('batteries', function() {
+    it('should have a `totalBatteries` variable', function() {
+      expect(totalBatteries).to.exist;
+    });
+
+    it('should have a number as a result', function() {
+      expect(totalBatteries).to.be.a('number');
+    });
+
+    it('should have made the sum of all the assembled batteries', function() {
+      expect(totalBatteries).to.eql(31);  // Expecting 31 as the total
+    });
+  });
+});
